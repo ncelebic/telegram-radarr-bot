3fe8870df3522b6c1d5e45ca1286bb99d87ca48f
eamondo2 <eamonbb@gmail.com>
2018-02-02 03:44:44 -0500

    fixing disk command

--- radarr.js
+++ radarr.js
@@ -392,6 +392,14 @@ bot.on('message', function(msg) {
         }
     }
 
+    if(/^\/disk\s?(@)(\S+)\s?(.+)?$/g.test(message)) {
+        if(isAuthorized(user.id)){
+            return radarr.getDiskUsage();
+        } else {
+            return replyWithError(user.id, new Error(i18n.__('notAuthorized')));
+        }
+    }
+
     if(/^\/rss\s?(@)(\S+)\s?(.+)?$/g.test(message)) {
         verifyAdmin(user.id);
         if(isAdmin(user.id)){
